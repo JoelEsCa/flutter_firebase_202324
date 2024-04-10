f44b0ee86a597673b29f3b36a7b427694837f0db
u0f <weedboy448@gmail.com>
2024-04-10 19:25:25 +0200

    Configuració accedir imatges dispositiu

--- lib/main.dart
+++ lib/main.dart
@@ -41,3 +41,50 @@ class MainApp extends StatelessWidget {
     - flutter pub add firebase_core
 
 */
+
+/*
+Imatges:
+========
+
+1) Habilitar Firebase Storage en el projecte vinculat de Firebase.
+  - Es pot posar les reglas a true el write i read.
+
+2) Descarreguem dependència de Firebase Storage al projecte.
+  - flutter pub add firebase_storage
+
+3) Desccarreguem una dependència per seleccionar imatges (un picker).
+  - N'hi ha diversos.
+  - Fem servir el image_picker
+    - flutter pub add image_picker
+
+4) Perquè funcioni en Android:
+  - Anar a android > app > src > main > AndroidManifest.xml
+    Escriure just abans del tag <aplication> (fora d'aplication):
+    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+    <uses-permission android:name="android.permission.READ_MEDIA_IMAGES"/>
+    <uses-permission android:name="android.permission.READ_MEDIA_VIDEO"/>
+
+    A més a més, seguramente caldrà anar a: android >  app > build.gradle:
+    - cambiar la minsdk a 21
+    
+5) Perquè funcioni en iOS:
+  - Anar a ios > Runner > Info.plist
+    - Afegir els permisos a les seguentes linies:
+      <key>NSPhotoLibraryUsageDescription</key>
+    <string>Privacy - Photo Library Usage Description</string>
+    <key>NSMotionUsageDescription</key>
+    <string>Motion usage description</string>
+    <key>NSPhotoLibraryAddUsageDescription</key>
+    <string>NSPhotoLibraryAddUsageDescription</string>
+
+6) Perquè funcioni en Web:
+  - Anar a web > index.html
+  - On diu "onEntrypoinLoaded"
+         onEntrypointLoaded: function(engineInitializer) {
+          engineInitializer.initializeEngine().then(function(appRunner) {
+            appRunner.runApp();
+          });
+        }
+  - Canviar-ho per:
+
+*/
